cb747dba1e335c072140e92e77a9ff659f2674e9
Jed Schmidt <jed@jedbook-air.local>
2013-04-11 14:45:29 -0400

    add tests

---
+++ test/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+phantomjs ../server.js 8000 &
+PHANTOM_PID=$!
+
+python -m SimpleHTTPServer 8001 &
+STATIC_PID=$!
+
+sleep 1
+
+RENDERED_HTML=`curl localhost:8000 -G -s --data-urlencode 'href=http://localhost:8001/#!/1'`
+
+kill -9 $PHANTOM_PID
+kill -9 $STATIC_PID
+
+if [[ "$RENDERED_HTML" =~ "<body>Page one</body>" ]]
+then
+    exit 0
+else
+    exit 1
+fi
